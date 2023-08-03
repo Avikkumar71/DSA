74ff86b691fb2c6285191246a3f1534fecffae47
Avik Kumar <117352212+Avikkumar71@users.noreply.github.com>
2023-08-03 14:16:48 +0530

    Add files via upload

---
+++ 0005-Sqrt(x)/Sqrt(x).cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+
+    long long int binarysearch( int x){
+        int start = 0;
+        int end = x;
+        long long int ans = -1;
+        long long int mid =  start + (end - start)/2;
+
+        while( start <= end){
+
+            long long int square = mid*mid;
+
+            if( square > x){
+                end = mid-1;
+            }
+            else if( square <x){
+                ans = mid;
+                start = mid+1;
+            }
+            else{
+                return mid;
+            }
+            mid =  start + (end - start)/2;
+        } 
+        return ans; 
+    }
+    int mySqrt(int x) {
+        return binarysearch(x);
+    }
+};
