4e890874876f90369e6bd56c187ad8361a0b2a89
Avik Kumar <117352212+Avikkumar71@users.noreply.github.com>
2023-08-03 14:24:06 +0530

    Add files via upload

---
+++ 0011-The K Weakest Rows in a Matrix/The K Weakest Rows in a Matrix.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) 
+    {
+        vector<int> ans;
+        vector<pair<int,int>> vp;
+        for(int i=0;i<mat.size();i++)
+        {
+            int count=0;
+            for(int j=0;j<mat[i].size();j++)
+            {
+                if(mat[i][j]==1)
+                count++;
+            }
+            vp.push_back(make_pair(count,i));
+        }
+        sort(vp.begin(),vp.end());
+        for(int i=0;i<k;i++)
+        {
+            ans.push_back(vp[i].second);
+        }
+        return ans;
+    }
+};
