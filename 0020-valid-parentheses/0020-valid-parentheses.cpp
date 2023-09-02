944c4dde2eb947ee845f8fbfdbf550e4eb8734e2
Avik Kumar <117352212+Avikkumar71@users.noreply.github.com>
2023-09-02 21:11:07 +0530

    Time: 3 ms (42.81%), Space: 6.3 MB (80.82%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        
+        stack <char> stc;
+        
+        for( int i=0; i<s.length(); i++){
+            char ch = s[i];
+            
+            if( ch == '(' || ch == '[' || ch == '{'){
+                stc.push(ch);
+            }
+            else{
+                if(!stc.empty() ){
+                    char top = stc.top();
+                    if( (ch == ')' && top == '(') || (ch == ']' && top == '[') ||
+                       (ch == '}' && top == '{') ){
+                        stc.pop();
+                    }
+                    else{
+                        return false;
+                    }
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        
+        if(stc.empty()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
